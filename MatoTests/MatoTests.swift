69f412833bff0f29b968d38d91b495a725a3d01a
The Ducktor <130664593+The-Ducktor@users.noreply.github.com>
2025-05-22 15:55:27 +0800

    Initial Commit

---
+++ MatoTests/MatoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatoTests.swift
+//  MatoTests
+//
+//  Created by Jackson Powell on 5/22/25.
+//
+
+import Testing
+@testable import Mato
+
+struct MatoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
